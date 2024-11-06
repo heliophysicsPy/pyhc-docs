8e8e691e29e66574453d149d8ed58b8e9a7deec3
Shawn Polson <sapols@yahoo.com>
2024-11-06 15:49:18 -0700

    Temporarily add debugging console messages

--- source/_static/auto_check_search.js
+++ source/_static/auto_check_search.js
@@ -1,6 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
+    console.log("auto_check_search.js loaded: attempting to check subproject checkbox.");
+
     const subprojectCheckbox = document.querySelector("#filter-0");
-    if (subprojectCheckbox && !subprojectCheckbox.checked) {
-        subprojectCheckbox.checked = true;
+    if (subprojectCheckbox) {
+        console.log("Checkbox found with id 'filter-0'. Current checked state:", subprojectCheckbox.checked);
+        if (!subprojectCheckbox.checked) {
+            subprojectCheckbox.checked = true;
+            console.log("Checkbox checked by auto_check_search.js.");
+        }
+    } else {
+        console.log("Checkbox with id 'filter-0' not found on this page.");
     }
 });
