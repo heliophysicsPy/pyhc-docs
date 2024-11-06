a2c1d45243538e0c9272ea9a2be4ef143cc8f9f2
Shawn Polson <sapols@yahoo.com>
2024-11-06 13:05:40 -0700

    Add intersphinx extension, link pysat and pyspedas

--- source/conf.py
+++ source/conf.py
@@ -1,5 +1,4 @@
 # Configuration file for the Sphinx documentation builder.
-#
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
@@ -13,15 +12,27 @@ author = 'PyHC'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+# Extensions for Sphinx
+extensions = [
+    'sphinx.ext.intersphinx',  # Enables linking to other projects
+]
 
+# Template and static paths
 templates_path = ['_templates']
 exclude_patterns = []
 
-
+# Intersphinx mappings for linking to PyHC subprojects
+intersphinx_mapping = {
+    'pysat': ('https://pysat.readthedocs.io/en/latest/', None),
+    'pyspedas': ('https://pyspedas.readthedocs.io/en/latest/', None),
+}
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
+# Theme options
 html_theme = 'alabaster'
 html_static_path = ['_static']
+
+# Additional configuration (optional)
+# Add any additional options you may need here as the project expands.
